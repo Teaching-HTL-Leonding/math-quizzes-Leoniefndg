a0f72747b3fb98aa589a37955581bccb0d5084fa
Leonie☀️ <113624408+Leoniefndg@users.noreply.github.com>
2022-12-01 11:57:32 +0100

    Add files via upload

---
+++ 030.Fizz_Buzz/Program.cs
@@ -0,0 +1,30 @@
+﻿    System.Console.WriteLine("Please enter the maximal number ");
+    int c = int.Parse(Console.ReadLine()!);
+
+    System.Console.WriteLine("please enter your first number ");
+    int a =int.Parse(Console.ReadLine()!);
+
+    System.Console.WriteLine("Please enter your secund number");
+    int b = int.Parse(Console.ReadLine()!);
+
+
+for (int n =1; n <= c; n++)
+{
+
+    if (n % a == 0 && n % b ==0)
+    {
+        System.Console.WriteLine(" FizzBuzz");
+    }
+    else if (n % a ==0)
+    {
+        System.Console.WriteLine(" Fizz");
+    }
+    else if (n % b ==0)
+    {
+        System.Console.WriteLine(" Buzz");
+    }
+    else 
+    {
+        System.Console.WriteLine(n);
+    }
+}
